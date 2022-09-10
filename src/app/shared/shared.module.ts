fd4e4273b86cb883e3f64338478cf751d091dbdb
Moi <moises.jimenez@rindus.de>
2022-09-10 10:19:33 +0200

    Add flex layout

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+  ]
+})
+export class SharedModule { }
